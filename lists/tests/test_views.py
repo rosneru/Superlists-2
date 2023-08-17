e0fdfa24d3f2f40f4b8986731f909e0c672fbe07
Uwe Rosner <oscottmusic@posteo.de>
2023-08-17 16:13:26 +0200

    Continue: 13.2: Checking That Invalid Input Isn’t Saved to the Database

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,3 @@
-
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -52,6 +51,13 @@ class NewListTest(TestCase):
                                     data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = "You can&#x27;t have an empty list item"
+        self.assertContains(response, expected_error)
 
 
 class NewItemTest(TestCase):
