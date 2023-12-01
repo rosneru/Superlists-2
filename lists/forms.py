a343205d9e4f5241613a16d024b45b70a7985660
Uwe Rosner <oscottmusic@posteo.de>
2023-12-01 15:27:12 +0100

    Now using a Django ModelForm (class Meta)
    Continue @ 14.1: Testing and Customising Form Validation

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,15 @@
 from django import forms
+from lists.models import Item
 
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder': 'Enter a TODO item',
-            'class': 'form-control input-lg',
-        })
-    )
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a TODO item',
+                'class': 'form-control input-lg',
+            }),
+        }
