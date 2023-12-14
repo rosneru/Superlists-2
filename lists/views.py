6d26c51fad961e318ffa98fccdab55e341acf933
Uwe Rosner <oscottmusic@posteo.de>
2023-12-14 10:07:19 +0100

    Use new form in home_page, simplify stuff, NB breaks stuff

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
 from django.forms import ValidationError
 from django.shortcuts import redirect, render
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
+
 
 def new_list(request):
     list_ = List.objects.create()
@@ -17,6 +19,7 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect(list_)
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
