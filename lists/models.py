8228e5df88fbae6048077c5f2477bfb5cedc7795
Uwe Rosner <oscottmusic@posteo.de>
2024-08-06 11:13:20 +0200

    Used Meta subclass to ensure validation
    Continue with "Rewriting the Old Model Test" halsfway in 15.1

--- lists/models.py
+++ lists/models.py
@@ -9,5 +9,8 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='', unique=True)
+    text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('list', 'text')
