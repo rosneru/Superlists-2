7a1496e941266630f299983c1916dc146cd05d53
Uwe Rosner <oscottmusic@posteo.de>
2024-10-17 14:17:01 +0200

    Added using of ExistingListItemForm and fixed UnitTests
    Continue
    
    @ 16.3 (ORDERING CHANGED) "A More Complex Form to Handle Uniqueness Validation"
    
    @ "But we still have something to do about our FTs:"

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, ExistingListItemForm, ItemForm
 from lists.models import Item, List
 from unittest import skip
 
@@ -115,13 +115,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         Item.objects.create(list=list1, text="textey")
@@ -130,12 +129,12 @@ class ListViewTest(TestCase):
             data={"text": "textey"},
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
-        list_ = List.objects.create()
-        response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        mylist = List.objects.create()
+        response = self.client.get(f'/lists/{mylist.id}/')
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text')
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import List
 
 
@@ -18,12 +18,14 @@ def new_list(request):
 
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    our_list = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=our_list)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=our_list, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
-            return redirect(list_)
+            form.save()
+            return redirect(our_list)
+    else:
+        form = ExistingListItemForm(for_list=our_list)
 
-    return render(request, 'list.html', {'list': list_, 'form': form})
+    return render(request, 'list.html', {'list': our_list, 'form': form})
